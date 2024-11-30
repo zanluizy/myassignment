948f70139c0cbec77540c4e4455293a976173c5b
zanluizy <zmokebisa@gmail.com>
2024-11-30 13:03:14 +0200

    new files

---
+++ inventory-frontend/build/precache-manifest.0ffa46def3bf3e2074430e2a6a6d00c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3caadb8971e0fa3160f6167edc6dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c183f1044df0908f73a",
+    "url": "/static/css/main.820d1d17.chunk.css"
+  },
+  {
+    "revision": "caac5c95dae5228eda25",
+    "url": "/static/js/2.75aedd42.chunk.js"
+  },
+  {
+    "revision": "5a8c50faf123b359b1c0",
+    "url": "/static/js/3.35fc77cc.chunk.js"
+  },
+  {
+    "revision": "4c183f1044df0908f73a",
+    "url": "/static/js/main.c2c0f200.chunk.js"
+  },
+  {
+    "revision": "ceb460f880afadc46175",
+    "url": "/static/js/runtime~main.b9ebbf7e.js"
+  },
+  {
+    "revision": "f320d94478de2733f5e91f1f3a0c8098",
+    "url": "/static/media/zen.f320d944.jpg"
+  }
+]);
